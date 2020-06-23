29e42fe8f9533d505c31d181aa1d77f0beb1bf19
LPattanaik <lagnajit@mit.edu>
2020-06-23 17:35:03 -0400

    restructured training code

--- train.py
+++ train.py
@@ -1,17 +1,17 @@
 from argparse import ArgumentParser
 import math
 import torch
-from tqdm import tqdm
 
-from model.G2C import CombinedNN
-from utils import create_logger, Standardizer
+from model.G2C import G2C
+from model.training import train, test, NoamLR
+from utils import create_logger
+from features.featurization import construct_loader
 
 
 parser = ArgumentParser()
 
 parser.add_argument('--log_dir', type=str)
-parser.add_argument('--data_path', type=str)
-parser.add_argument('--confs_dir', type=str)
+parser.add_argument('--sdf_dir', type=str)
 parser.add_argument('--split_path', type=str)
 
 parser.add_argument('--n_epochs', type=int, default=100)
@@ -29,51 +29,19 @@ logger = create_logger('train', args.log_dir)
 
 
 
-def train(model, loader, optimizer, loss, stdzer, device):
-    model.train()
-    loss_all = 0
-
-    for data in tqdm(loader):
-        data = data.to(device)
-        optimizer.zero_grad()
-
-        out = model(data)
-        result = loss(out, stdzer(data.y))
-        result.backward()
-
-        # clip the gradients
-        torch.nn.utils.clip_grad_norm_(net.parameters(), max_norm=10, norm_type=2)
-
-        optimizer.step()
-        loss_all += loss(stdzer(out, rev=True), data.y)
-
-    return math.sqrt(loss_all / len(loader.dataset))  # rmse
-
-def test(model, loader, loss, stdzer, device):
-    model.eval()
-    error = 0
-
-    for data in tqdm(loader):
-        data = data.to(device)
-        out = model(data)
-        error += loss(stdzer(out, rev=True), data.y).item()
 
 
 train_loader, val_loader = construct_loader(args)
-mean = train_loader.dataset.mean
-std = train_loader.dataset.std
-stdzer = Standardizer(mean, std)
-
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = CombinedNN()().to(device)
+model = G2C(train_loader.dataset.num_node_features, train_loader.dataset.num_edge_features, 100, 3, 2).to(device)
 if torch.cuda.device_count() > 1:
     model = torch.nn.DataParallel(model)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-# scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
-#                                                        factor=0.7, patience=5,
-#                                                        min_lr=0.00001)
-#todo: Do we want to use a lr scheduler?
-scheduler = build_lr_scheduler(optimizer, args, len(train_loader.dataset))
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
+                                                       factor=0.7, patience=5,
+                                                       min_lr=0.00001)
+# todo: Do we want to use a lr scheduler?
+# scheduler = build_lr_scheduler(optimizer, args, len(train_loader.dataset))
 loss = torch.nn.MSELoss(reduction='sum')
 # or use MAE loss
 # todo access mask from data.batch attribute to reflect the previous implementation
@@ -85,12 +53,13 @@ best_epoch = 0
 
 logger.info("Starting training...")
 for epoch in range(1, args.n_epochs):
-    train_loss = train(model, train_loader, optimizer, loss, stdzer, device)
+    train_loss = train(model, train_loader, optimizer, loss, device, scheduler)
     logger.info("Epoch {}: Training Loss {}".format(epoch, train_loss))
 
-    val_loss = test(model, val_loader, loss, stdzer, device)
+    val_loss = test(model, val_loader, loss, device)
     logger.info("Epoch {}: Validation Loss {}".format(epoch, val_loss))
-    scheduler.step()
+    if not isinstance(scheduler, NoamLR):
+        scheduler.step()
 
     if val_loss <= best_val_loss:
         best_val_loss = val_loss
