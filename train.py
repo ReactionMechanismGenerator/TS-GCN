2837e0e6e2f000159dc3241997aa0c23d3b54996
Kevin Spiekermann <kspieker@mit.edu>
2020-06-28 14:16:44 -0400

    Added command line arguments to replace previously hardcoded values
    Also logged model parameters for easier comparison later

--- train.py
+++ train.py
@@ -23,6 +23,10 @@ parser.add_argument('--mini_batch', type=int, default=4)
 parser.add_argument('--lr', type=float, default=1e-3)
 parser.add_argument('--num_workers', type=int, default=2)
 
+parser.add_argument('--hidden_dim', type=int, default=300)
+parser.add_argument('--depth', type=int, default=3)
+parser.add_argument('--n_layers', type=int, default=2)
+
 parser.add_argument('--atom_messages', action='store_true')
 parser.add_argument('--use_cistrans_messages', action='store_true')
 
@@ -30,19 +34,31 @@ args = parser.parse_args()
 log_file_name = datetime.today().isoformat() + '_train'
 logger = create_logger(log_file_name, args.log_dir)
 
-
+logger.info('Arguments are...')
+for arg in vars(args):
+    logger.info(f'{arg}: {getattr(args, arg)}')
 
 
 
 train_loader, val_loader = construct_loader(args)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = G2C(train_loader.dataset.num_node_features, train_loader.dataset.num_edge_features, 100, 3, 2).to(device)
+model = G2C(train_loader.dataset.num_node_features, train_loader.dataset.num_edge_features,
+            args.hidden_dim, args.depth, args.n_layers).to(device)
 if torch.cuda.device_count() > 1:
+    logger.info(f'Using {torch.cuda.device_count()} GPUs for training...')
     model = torch.nn.DataParallel(model)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
                                                        factor=0.7, patience=5,
                                                        min_lr=0.00001)
+# record parameters
+logger.info(f'\nModel architecture is:\n{model}\n')
+logger.info(f'Optimizer parameters are:\n{optimizer}\n')
+logger.info(f'Scheduler state dict is:')
+for key, value in scheduler.state_dict().items():
+    logger.info(f'{key}: value')
+logger.info('')
+
 # alternative lr scheduler
 # scheduler = build_lr_scheduler(optimizer, args, len(train_loader.dataset))
 loss = torch.nn.MSELoss(reduction='sum')
