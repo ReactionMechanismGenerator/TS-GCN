88fb752bbb6d77b8298f0032e2ae6379bb3dd4bb
LPattanaik <lagnajit@mit.edu>
2020-07-20 17:09:21 -0400

    changed default batch size and hidden dim

--- train.py
+++ train.py
@@ -18,12 +18,12 @@ parser.add_argument('--split_path', type=str)
 
 parser.add_argument('--n_epochs', type=int, default=100)
 parser.add_argument('--warmup_epochs', type=int, default=2)
-parser.add_argument('--batch_size', type=int, default=64)
+parser.add_argument('--batch_size', type=int, default=16)
 parser.add_argument('--mini_batch', type=int, default=4)
 parser.add_argument('--lr', type=float, default=1e-3)
 parser.add_argument('--num_workers', type=int, default=2)
 
-parser.add_argument('--hidden_dim', type=int, default=300)
+parser.add_argument('--hidden_dim', type=int, default=100)
 parser.add_argument('--depth', type=int, default=3)
 parser.add_argument('--n_layers', type=int, default=2)
 
