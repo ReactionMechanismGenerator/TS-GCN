cf70df8a09c887fb7782fd1324a4927a4e46a556
LPattanaik <lagnajit@mit.edu>
2020-07-20 17:04:47 -0400

    added custom MetaLayer

--- model/GNN.py
+++ model/GNN.py
@@ -1,11 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
 
 from torch.nn import Sequential as Seq, Linear as Lin, ReLU
 from torch_scatter import scatter_sum
-from torch_geometric.nn import MetaLayer
 
 
 class MLP(nn.Module):
@@ -38,46 +36,31 @@ class MLP(nn.Module):
         return x
 
 
-
-class Module2(nn.Module):
+class MetaLayer(torch.nn.Module):
+    r"""A meta layer for building any kind of graph network, inspired by the
+    `"Relational Inductive Biases, Deep Learning, and Graph Networks"
+    <https://arxiv.org/abs/1806.01261>`_ paper.
     """
-    Creates a NN using nn.ModuleList to automatically adjust the number of layers.
-    Allows the user to customize the number of nodes at each hidden layer.
+    def __init__(self, edge_model=None, node_model=None):
+        super(MetaLayer, self).__init__()
+        self.edge_model = edge_model
+        self.node_model = node_model
 
-    Inputs:
-        h_sizes (list):             number of features input to each layer. 
-                                    length of h_sizes will determine the number of layers.
-        out_dim (int):              number of features in the final output from the network.
-        activation (torch function): activation function to be used during the hidden layers
+        self.reset_parameters()
 
-    """
-    def __init__(self, h_sizes, out_dim, activation=torch.nn.ReLU()):
-        super(Module2, self).__init__()
-        self.layers = nn.ModuleList()
-        num_layers = len(h_sizes)
-        
-        # create hidden layers
-        for k in range(num_layers - 1):
-            self.layers.append(nn.Linear(h_sizes[k], h_sizes[k+1]))
-            self.layers.append(activation)
-        
-        # create output layer
-        # could also just have out_dim be the last entry in h_sizes
-        self.layers.append(nn.Linear(h_sizes[-1], out_dim))
-        # self.layers.append(nn.Linear(dIn, dOut))  # linear output
-        self.layers.append(torch.nn.Sigmoid())    # sigmoid output
-        # self.layers.append(torch.nn.Tanh())       # tanh output
-        
-    def forward(self, x):
-        y = x
-        for i in range(len(self.layers)):
-            y = self.layers[i](y)
-        return y
+    def reset_parameters(self):
+        for item in [self.node_model, self.edge_model]:
+            if hasattr(item, 'reset_parameters'):
+                item.reset_parameters()
 
-# test the MLP
-# mlp = Module2([2, 5, 55, 6, 6, 7], 5)
-# print(mlp)
-# print(mlp(torch.from_numpy(np.array([4,5])).float()))
+    def forward(self, x, edge_index, edge_attr=None, batch=None):
+        """"""
+        if self.edge_model is not None:
+            edge_attr = edge_attr + self.edge_model(x, edge_attr, edge_index)
+        if self.node_model is not None:
+            x = x + self.node_model(x, edge_index, edge_attr, batch)
+
+        return x, edge_attr
 
 
 class EdgeModel(nn.Module):
@@ -88,17 +71,18 @@ class EdgeModel(nn.Module):
         self.node_out = Lin(hidden_dim, hidden_dim)
         self.mlp = MLP(hidden_dim, hidden_dim, n_layers)
 
-    def forward(self, src, tgt, edge_attr, u, batch):
+    def forward(self, x, edge_attr, edge_index):
         # source, target: [2, E], where E is the number of edges.
         # edge_attr: [E, F_e]
         # u: [B, F_u], where B is the number of graphs (we don't have any of these yet)
         # batch: [E] with max entry B - 1.
 
         f_ij = self.edge(edge_attr)
-        f_i = self.node_in(src)
-        f_j = self.node_out(tgt)
+        f_i = self.node_in(x)
+        f_j = self.node_out(x)
+        row, col = edge_index
 
-        out = F.relu(f_ij + f_i + f_j)
+        out = F.relu(f_ij + f_i[row] + f_j[col])
         return self.mlp(out)
 
 
@@ -108,7 +92,7 @@ class NodeModel(nn.Module):
         self.node_mlp_1 = MLP(hidden_dim, hidden_dim, n_layers)
         self.node_mlp_2 = MLP(hidden_dim, hidden_dim, n_layers)
 
-    def forward(self, x, edge_index, edge_attr, u, batch):
+    def forward(self, x, edge_index, edge_attr, batch):
         # x: [N, h], where N is the number of nodes.
         # edge_index: [2, E] with max entry N - 1.
         # edge_attr: [E, F_e]
@@ -134,5 +118,5 @@ class GNN(nn.Module):
         x = self.node_init(x)
         edge_attr = self.edge_init(edge_attr)
         for _ in range(self.depth):
-            x, edge_attr, _ = self.update(x, edge_index, edge_attr)
+            x, edge_attr = self.update(x, edge_index, edge_attr)
         return x, edge_attr
