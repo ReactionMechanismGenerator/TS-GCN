37d8c3973ff2b216d0f4d66210b23eb6b59285a8
Kevin Spiekermann <kspieker@mit.edu>
2021-01-02 08:42:10 -0500

    Update env name to match new repo name

--- model/common.py
+++ model/common.py
@@ -2,4 +2,4 @@ import os
 
 
 # absolute path to the repo's home folder
-ts_gen_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+ts_gcn_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
