700791a46bd35ea9e68297afbb0b3387b310c460
Calvin <calvinpieters@gmail.com>
2023-01-02 12:05:43 +0200

    Updated create_env_cpu tool with mamba check
    Added an if/elif statement to check for mamba or conda. If neither are found, then the user will be prompted to install either.

--- devtools/create_env_cpu.sh
+++ devtools/create_env_cpu.sh
@@ -24,9 +24,20 @@ then
 fi  
 CUDA_VERSION="cpu"
 
+#Checking for package manager
+if [ -x "$(command -v mamba)" ]; then
+	echo "mamba is installed."
+	COMMAND_PKG=mamba
+elif [ -x "$(command -v conda)" ]; then
+	echo "conda is installed."
+	COMMAND_PKG=conda
+else
+    echo "mamba and conda are not installed. Please download and install mamba or conda - we strongly recommend mamba"
+fi
+
 echo "Creating conda environment..."
-echo "Running: conda create -n ts_gcn python=3.7.11 -y"
-conda create -n ts_gcn python=3.7.11 -y
+echo "Running: $COMMAND_PKG create -n ts_gcn python=3.7.11 -y"
+$COMMAND_PKG create -n ts_gcn python=3.7 -y
 
 # activate the environment to install torch-geometric
 echo "Checking which python"
@@ -39,8 +50,9 @@ conda activate ts_gcn
 echo "Checking which python"
 which python
 
-echo "Running: conda install pytorch==1.7.1 cpuonly -c pytorch -y"
-conda install pytorch==1.7.1 cpuonly -c pytorch -y
+
+echo "Running: $COMMAND_PKG install pytorch==1.7.1 cpuonly -c pytorch -y"
+$COMMAND_PKG install pytorch==1.7.1 cpuonly -c pytorch -y
 
 echo "Installing torch-scatter and torch-sparse..."
 pip install --verbose --no-cache-dir torch-scatter==2.0.7 torch-sparse==0.6.9 -f https://data.pyg.org/whl/torch-1.7.1+cpu.html
@@ -49,5 +61,5 @@ echo "Installing torch-geometric..."
 pip install torch-geometric
 
 echo "Installing other dependencies..."
-conda env update -f cpu_environment.yml
+$COMMAND_PKG env update -f cpu_environment.yml
 
